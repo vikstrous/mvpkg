dee931a95ee052efc92f901e0d9104e78a169f62
Viktor Stanchev <me@viktorstanchev.com>
2020-02-29 14:14:26 -0800

    handle aliased references

---
+++ internal/mvpkg/expected-recursive/destination/alias.go
@@ -0,0 +1,9 @@
+package destination
+
+import (
+	aliased "example.com/destination/testpkg2"
+)
+
+func exampleFunc2() {
+	aliased.ExampleFunc()
+}
---
+++ internal/mvpkg/testtemplate/destination/alias.go
@@ -0,0 +1,9 @@
+package destination
+
+import (
+	aliased "example.com/source/testpkg"
+)
+
+func exampleFunc2() {
+	aliased.ExampleFunc()
+}
