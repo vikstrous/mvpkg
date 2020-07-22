103a82c903261e29bc86a90002203f98d3736aad
Viktor Stanchev <me@viktorstanchev.com>
2020-07-22 12:07:06 -0400

    add new test pattern

--- internal/mvpkg/mvpkg_test.go
+++ internal/mvpkg/mvpkg_test.go
@@ -2,8 +2,12 @@ package mvpkg_test
 
 import (
 	"bytes"
+	"encoding/json"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/vikstrous/mvpkg/internal/mvpkg"
@@ -39,6 +43,67 @@ func compare(t testing.TB, expected, actual string) {
 	err := cmd.Run()
 	if err != nil {
 		t.Fatalf("test comparison failed: %s\n%s\n%s\n", err, stdout.String(), stderr.String())
+	}
+}
+
+func TestGeneric(t *testing.T) {
+	testsDir := "tests"
+
+	testFiles, err := ioutil.ReadDir(testsDir)
+	if err != nil {
+		t.Fatalf("failed to read list of tests: %s", err)
+	}
+
+	for _, testFile := range testFiles {
+		if !testFile.IsDir() {
+			continue
+		}
+		// Drop the number from the name
+		testSrcDir := filepath.Join(testsDir, testFile.Name())
+		testName := strings.Join(strings.Split(testFile.Name(), "_")[1:], "_")
+
+		// clean up before starting and between tests
+		cleanup()
+		t.Run(testName, func(t *testing.T) {
+			originalPath := filepath.Join(testSrcDir, "original")
+			// using a binary dependency rather than a library one
+			err := exec.Command("cp", "-r", originalPath, testDir).Run()
+			if err != nil {
+				t.Fatalf("failed to create test dir running: %s", err)
+			}
+			defer func() {
+				// leave the resulting failed output directory in place if the tests failed so we can inspect it
+				if !t.Failed() {
+					cleanup()
+				}
+			}()
+
+			expectedPath := filepath.Join(testSrcDir, "expected")
+			testInfoFilename := filepath.Join(testSrcDir, "testInfo.json")
+			testInfoStr, err := ioutil.ReadFile(testInfoFilename)
+			if err != nil {
+				t.Fatalf("failed to read command file from %s: %s", testInfoFilename, err)
+			}
+			var testInfo struct {
+				PWD         string   `json:"pwd"`
+				Source      string   `json:"source"`
+				Destination string   `json:"destination"`
+				BuildFlags  []string `json:"build_flags"`
+			}
+			err = json.Unmarshal(testInfoStr, &testInfo)
+			if err != nil {
+				t.Fatalf("failed to JSON unmarshal file from %s: %s", testInfoFilename, err)
+			}
+
+			// run the tool
+			err = mvpkg.MvPkg(t.Logf, filepath.Join(testDir, testInfo.PWD), testInfo.Source, testInfo.Destination, testInfo.BuildFlags, false, false)
+			if err != nil {
+				t.Fatalf("MvPkg fialed: %s", err)
+			}
+
+			// validate the results
+			compare(t, expectedPath, testDir)
+		})
 	}
 }
 
---
+++ internal/mvpkg/tests/01_trivial/README.md
@@ -0,0 +1,5 @@
+# Trivial
+
+This tests makes sure that moving a package with no imports works.
+
+We move ./source/target to ./destination/target without changing the name of the target package.
